71ddb5e099c4a5bdf5b8e4525dd69b40d5950d48
HugoA23 <143725565+HugoA23@users.noreply.github.com>
2023-10-24 07:42:40 -0600

    Add files via upload

---
+++ U4Arreglos.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package u4arreglos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class U4Arreglos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        boolean[] banderas = {true,false,true,true,false};
+        String[] nombres = {"cesar","mateo","Julio","Victor","Panchito","cris","liz","fatima","carlos","Jacky","Josue", "Zoe","Issac","Gahel"};
+        double[] calificaciones = {8.3,6.4,5.6,6.9,7.8,10.0,9.8,7.8,9.4,5.6};
+        char[] vocales;
+        
+        for (int i = 0; i < 10; i++) {
+            
+        }
+        // TODO code application logic here
+    }
+    
+}
